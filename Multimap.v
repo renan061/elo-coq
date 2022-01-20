068bcb015f56827d79087e761da85d45353f5ea2
Renan Santos <renan.061@gmail.com>
2022-01-20 05:10:03 -0300

    Working on trace_preservation

--- Multimap.v
+++ Multimap.v
@@ -1,3 +1,4 @@
+From Coq Require Import Lists.List.
 
 Local Definition map' (K V : Type) :=
   prod (K -> V) (K -> K -> bool).
@@ -7,20 +8,25 @@ Local Definition empty' {K V : Type} v eqb : map' K V := (fun _ => v, eqb).
 Local Definition update' {K V : Type} (m : map' K V) k v :=
   (fun k' => if (snd m) k k' then v else (fst m) k', snd m).
 
+Local Definition map (K V : Type) := map' K (option V).
 
-Definition map (K V : Type) := map' K (option V).
+Local Definition empty {K V : Type} eqb : map K V := empty' None eqb.
 
-Definition empty {K V : Type} eqb : map K V := empty' None eqb.
-
-Definition update {K V : Type} (m : map K V) k v :=
+Local Definition update {K V : Type} (m : map K V) k v :=
   update' m k (Some v).
 
-Definition lookup {K V : Type} (m : map K V) k := (fst m) k.
+Local Definition lookup {K V : Type} (m : map K V) k := (fst m) k.
 
+Definition multimap (K V : Type) := map' K (list V).
 
-Definition multimap (K V : Type) := map K (map' V V).
+Definition multimap_empty {K V : Type} eqb : multimap K V :=
+  empty' nil eqb.
 
-(*
-Definition multimap_empty {K V : Type} eqbk eqbv :=
-  empty' (empty  V) nil eqb.
-*)
+Definition multimap_lookup {K V : Type} (m : multimap K V) k : list V :=
+  fst m k.
+
+Definition multimap_contains {K V : Type} (m : multimap K V) k v : Prop :=
+  In v (multimap_lookup m k).
+
+Definition multimap_update {K V : Type} (m : multimap K V) k v :=
+  update' m k (v :: multimap_lookup m k).
--- Safety.v
+++ Safety.v
@@ -1,56 +1,131 @@
 From Coq Require Import Init.Nat.
+From Coq Require Import Arith.Arith.
 From Coq Require Import Lists.List.
 
+From Elo Require Export Array.
 From Elo Require Export Multimap.
 From Elo Require Export Core.
 From Elo Require Export Properties.
 
-Reserved Notation "m / ths '==>*' memvis / trace"
-  (at level 40, ths at next level, memvis at next level).
-Reserved Notation "'|==' trace"
-  (at level 40).
+Reserved Notation "m / ths / mvis / trc '~~>' m' / ths' / mvis' / trc'"
+  (at level 40, ths at next level, mvis at next level, trc at next level,
+                m' at next level, ths' at next level, mvis' at next level).
 
-Definition trace := list ceffect.
-(* memory visibility => maps a memory address to a thread *)
+(* memory visibility *)
+(* maps a memory address to the threads that can access it *)
 Definition memvis := multimap nat nat.
+Definition trace := list ceffect.
 
-Inductive trace_cmultistep : mem -> threads -> memvis -> trace -> Prop :=
-  | trace_cmultistep_one : forall ths,
-    well_typed_program nil nil ths ->
-    nil / ths ==>* multimap_empty / (CEF_None 0 :: nil)
+Definition memvis_empty : memvis := multimap_empty eqb.
 
-  | trace_cmultistep_none : forall i m m' ths ths' mvis trace,
-    m  / ths  ==>* mvis / trace ->
-    m  / ths  ==>  m' / ths' # (CEF_None i) ->
-    m' / ths' ==>* mvis / (CEF_None i :: trace)
+Definition memvis_update mvis ceff :=
+  match ceff with
+  | CEF_None  tid => mvis
+  | CEF_Alloc tid addr => multimap_update mvis addr tid
+  | CEF_Load  tid addr => mvis
+  | CEF_Store tid addr => mvis
+  | CEF_Spawn tid => mvis (* TODO *)
+  end.
 
-  | trace_cmultistep_alloc : forall i addr m m' ths ths' mvis trace,
-    m  / ths  ==>* mvis / trace ->
-    m  / ths  ==>  m' / ths' # (CEF_Alloc i addr) ->
-    m' / ths' ==>* (add mvis i) / (CEF_Alloc i addr :: trace)
+(* trace step (from one concurrent state to another) *)
+Inductive tstep : mem -> threads -> memvis -> trace ->
+                  mem -> threads -> memvis -> trace -> Prop :=
+  | trace_refl : forall m ths mvis trc,
+    m / ths / mvis / trc ~~> m / ths / mvis / trc
 
-  | trace_cmultistep_load : forall i addr m m' ths ths' mvis trace,
-    m  / ths  ==>* mvis / trace ->
-    m  / ths  ==>  m' / ths' # (CEF_Load i addr) ->
-    m' / ths' ==>* mvis / (CEF_Load i addr :: trace)
+  | trace_step : forall m0 m m' ths0 ths ths' mvis0 mvis trc0 trc ceff,
+    m0 / ths0 / mvis0 / trc0 ~~> m / ths / mvis / trc ->
+    m / ths ==> m' / ths' # ceff ->
+    m / ths / mvis / trc ~~> m' / ths' / (memvis_update mvis ceff) / (ceff :: trc)
 
-  | trace_cmultistep_store : forall i addr m m' ths ths' mvis trace,
-    m  / ths  ==>* mvis / trace ->
-    m  / ths  ==>  m' / ths' # (CEF_Store i addr) ->
-    m' / ths' ==>* mvis / (CEF_Store i addr :: trace)
-(*
-  | trace_cmultistep_spawn : forall i m m' ths ths' mvis trace,
-    m  / ths  ==>* mvis / trace ->
-    m  / ths  ==>  m' / ths' # (CEF_Spawn i) ->
-    m' / ths' ==>* mvis / (CEF_Spawn i :: trace)
-*)
-  where "m / ths '==>*' mvis / trace" := (trace_cmultistep m ths mvis trace).
+  where "m / ths / mvis / trc '~~>' m' / ths' / mvis' / trc'" :=
+    (tstep m ths mvis trc m' ths' mvis' trc').
 
-Lemma well_typed_trace_cmultistep : forall m ths mvis trace,
-  m / ths ==>* mvis / trace ->
-  exists mt, well_typed_program mt m ths.
+Definition safe_memory_address mt addr :=
+  safe_type (get_typ mt addr).
+
+Definition safe_load mt (mvis : memvis) tid addr :=
+  multimap_lookup mvis addr = (tid :: nil) \/
+  safe_memory_address mt addr.
+
+Definition safe_store (mvis : memvis) tid addr :=
+  multimap_lookup mvis addr = tid :: nil.
+
+Inductive well_formed_trace : mem_typ -> memvis -> trace -> Prop :=
+  | well_formed_trace_nil : forall mt, (* TODO: is this mt correct? *)
+    well_formed_trace mt memvis_empty nil
+
+  | well_formed_trace_none : forall mt mvis trc tid,
+    well_formed_trace mt mvis trc ->
+    well_formed_trace mt mvis (CEF_None tid :: trc)
+
+  | well_formed_trace_alloc : forall mt mt' T mvis mvis' trc tid addr,
+    mt' = add mt T ->
+    mvis' = multimap_update mvis addr tid ->
+    well_formed_trace mt mvis trc ->
+    well_formed_trace mt' mvis' (CEF_Alloc tid addr :: trc)
+
+  | well_formed_trace_load : forall mt mvis trc tid addr,
+    safe_load mt mvis tid addr ->
+    well_formed_trace mt mvis trc ->
+    well_formed_trace mt mvis (CEF_Load tid addr :: trc)
+
+  | well_formed_trace_store : forall mt mvis trc tid addr,
+    safe_store mvis tid addr ->
+    well_formed_trace mt mvis trc ->
+    well_formed_trace mt mvis (CEF_Store tid addr :: trc)
+
+  | well_formed_trace_spawn : forall mt mvis trc tid,
+    well_formed_trace mt mvis trc ->
+    well_formed_trace mt mvis (CEF_Spawn tid :: trc) (* TODO: different mvis *)
+  .
+
+(* TODO: move *)
+Theorem cstep_none_preservation : forall mt m ths ths' tid,
+  well_typed_program mt m ths ->
+  m / ths ==> m / ths' # (CEF_None tid) ->
+  well_typed_program mt m ths'.
 Proof.
-  intros * H. induction H;
+  intros * [Hmem Hths] Hcstep.
+  inversion Hcstep; subst. split; trivial.
+  intros tid'. destruct (tid =? tid') eqn:E.
+  + apply Nat.eqb_eq in E. subst.
+    rewrite (get_i_set_i TM_Nil); trivial.
+    specialize (Hths tid') as [? ?]. eexists.
+    eauto using limited_preservation.
+  + apply Nat.eqb_neq in E. apply not_eq_sym in E.
+    rewrite (get_i_set_j TM_Nil); trivial.
+Qed.
+
+Theorem trace_preservation : forall mt m m' ths ths' mvis mvis' trc trc',
+  well_typed_program mt m ths ->
+  well_formed_trace mt mvis trc ->
+  m / ths / mvis / trc ~~> m' / ths' / mvis' / trc' ->
+  exists mt' mvis',
+    mt' extends mt /\
+    well_formed_trace mt' mvis' trc'.
+Proof.
+  intros * Hwt Htrace Htstep. inversion Htstep; subst.
+  - eexists. eexists. split; eauto using extends_refl.
+  - unfold memvis_update in *. destruct ceff.
+    + inversion H0; subst. eauto.
+      assert (well_typed_program mt m' (set ths i t)) as [? Hwt'].
+      eauto using cstep_none_preservation.
+      eexists. eexists. split; eauto using extends_refl, well_formed_trace.
+    + eexists. eexists. split; eauto using extends_add.
+      eapply well_formed_trace_alloc; eauto.
+    + eexists. eexists. split; eauto using extends_refl.
+      eapply well_formed_trace_load; eauto.
+      (* TODO *)
+Admitted.
+
+
+(*
+Lemma well_typed_cmultistep : forall m m' ths ths' mvis trc,
+  m / ths ==>* m' / ths' # mvis # trc ->
+  exists mt, well_typed_program mt m' ths'.
+Proof.
+  intros * Hcmultistep. induction Hcmultistep;
   try solve [
     match goal with
     | IH : exists mt, _ |- _ =>
@@ -62,79 +137,60 @@ Proof.
     eauto using preservation
   ].
   exists nil. split.
-  + split; trivial. destruct i; eauto using @typeof.
-  + intros i. specialize H as [_ Hths]. specialize (Hths i). assumption.
+  - split; trivial. destruct i; eauto using @typeof.
+  - intros i. unfold get_tm. simpl.
+    repeat (destruct i; eauto using @typeof).
 Qed.
+*)
 
-Fixpoint trace_has_concurrent_load trace i addr :=
-  match trace with
-  | nil =>
-      False
-  | CEF_Load j addr :: trace =>
-      i <> j \/ (trace_has_concurrent_load trace i addr)
-  | _ :: trace =>
-      trace_has_concurrent_load trace i addr
-  end.
-
-Fixpoint trace_has_concurrent_store trace i addr :=
-  match trace with
-  | nil =>
-      False
-  | CEF_Store j addr :: trace =>
-      i <> j \/ (trace_has_concurrent_store trace i addr)
-  | _ :: trace =>
-      trace_has_concurrent_store trace i addr
-  end.
-
-Inductive well_formed_trace : trace -> Prop :=
-  | well_formed_trace_nil :
-    |== nil
-
-  | well_formed_trace_none : forall i trace,
-    |== trace ->
-    |== (CEF_None i :: trace)
-
-  | well_formed_trace_alloc : forall i addr trace,
-    |== trace ->
-    |== (CEF_Alloc i addr :: trace)
-
-  | well_formed_trace_load : forall i addr trace,
-    ~ (trace_has_concurrent_load trace i addr) ->
-    |== trace ->
-    |== (CEF_Load i addr :: trace)
-
-  | well_formed_trace_store : forall i addr trace,
-    ~ (trace_has_concurrent_load trace i addr) ->
-    ~ (trace_has_concurrent_store trace i addr) ->
-    |== trace ->
-    |== (CEF_Store i addr :: trace)
-
-  | well_formed_trace_spawn : forall i trace,
-    |== trace ->
-    |== (CEF_Spawn i :: trace)
-
-  where "'|==' trace" := (well_formed_trace trace).
+(*
+Lemma aux : forall mt m0 m m' ths0 ths ths' mvis trc tid addr,
+  m0 / ths0 ==>* m / ths # mvis # trc ->
+  well_typed_memory mt m ->
+  mt |== trc ->
+  m / ths ==> m' / ths' # CEF_Load tid addr ->
+  exists mt',
+    well_typed_memory mt' m' /\
+    mt' |== (CEF_Load tid addr :: trc).
+Proof.
+  intros * Hcmultistep Hmem Htrace Hcstep.
+  eexists mt. split.
+  - inversion Hcstep. subst. assumption.
+  - eapply well_formed_trace_load; eauto.
+    unfold safe_load.
+Admitted.
 
 Theorem traces_are_well_formed : forall m ths mvis trace,
   m / ths ==>* mvis / trace ->
   |== trace.
 Proof.
+  intros * Hcsteps. induction Hcsteps; eauto using well_formed_trace.
+  - eapply well_formed_trace_load; eauto.
+  - eapply well_formed_trace_store; eauto.
 Admitted.
+*)
+
+(* FOR LATER *)
 
 (*
-
-  intros ? ? trace. induction trace as [| ceff trace IH]; intros * Hcsteps.
-  - inversion Hcsteps.
-  - assert (exists mt, well_typed_program mt m ths) as [mt [Hmem Hths]].
-    { eauto using well_typed_trace_cmultistep. }
-    inversion Hcsteps; subst;
-    try destruct ceff; eauto using well_formed_trace.
-    + eapply well_formed_trace_load; eauto.
-      specialize (Hths i) as [T Htype].
-      shelve.
-    + eapply well_formed_trace_store; eauto.
-      * shelve.
-      * shelve.
-Admitted.
-
+Lemma well_typed_cmultistep : forall m m' ths ths' mvis trc,
+  m / ths ==>* m' / ths' # mvis # trc ->
+  exists mt, well_typed_program mt m' ths'.
+Proof.
+  intros * Hcmultistep. induction Hcmultistep;
+  try solve [
+    match goal with
+    | IH : exists mt, _ |- _ =>
+      specialize IH as [mt ?]
+    end;
+    assert (exists mt',
+      mt' extends mt /\
+      well_typed_program mt' m' ths') as [? [_ ?]];
+    eauto using preservation
+  ].
+  exists nil. split.
+  - split; trivial. destruct i; eauto using @typeof.
+  - intros i. unfold get_tm. simpl.
+    repeat (destruct i; eauto using @typeof).
+Qed.
 *)
