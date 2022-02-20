e15cc1076c454cf2179a3004c6f9f7046ab6230d
Renan Santos <renan.061@gmail.com>
2022-02-20 04:06:25 -0300

    Simplifying the language

--- Core.v
+++ Core.v
@@ -15,88 +15,41 @@ Reserved Notation "'[' id ':=' x ']' t"
   (at level 20, id constr).
 Reserved Notation "t '--[' eff ']-->' t'"
   (at level 40).
-Reserved Notation "m / ths '==>' m' / ths' # ceff"
-  (at level 40, ths at next level, m' at next level).
-Reserved Notation "Gamma '|--' t 'is' T"
-  (at level 40, t at next level).
+Reserved Notation "m / ths '==[' ceff ']==>' m' / ths'"
+  (at level 40, ths at next level, ceff at next level, m' at next level).
 Reserved Notation "mt / Gamma '|--' t 'is' T"
   (at level 40, Gamma at next level).
 
 (* Types *)
 
 Inductive typ : Set :=
-  (* primitive types *)
   | TY_Void
   | TY_Num
-  | TY_Arr : typ -> typ
-  | TY_IArr : typ -> typ
-  (* internal types *)
   | TY_Ref : typ -> typ
-  | TY_IRef : typ -> typ
   | TY_Fun : typ -> typ -> typ
   .
-
-(* Type Safety *)
-
-Inductive safe_type : typ -> Prop :=
-  | SF_Void : safe_type TY_Void
-  | SF_Num  : safe_type TY_Num
-  | SF_IArr : forall T, safe_type T -> safe_type (TY_IArr T)
-  | SF_IRef : forall T, safe_type T -> safe_type (TY_IRef T)
-  | SF_Fun  : forall P R, safe_type (TY_Fun P R)
-  .
-
-Fixpoint is_safe_type T :=
-  match T with
-  | TY_Void
-  | TY_Num
-  | TY_Fun _ _ => true
-  | TY_IArr T'
-  | TY_IRef T' => is_safe_type T'
-  | _ => false
-  end.
-
-Definition safe (Gamma : map typ) :=
-  fun k => 
-    match Gamma k with
-    | None => None
-    | Some T => if is_safe_type T then Some T else None
-    end.
 
 (* Terms *)
 
 Inductive tm : Set :=
-  (* expressions *)
   | TM_Nil
   | TM_Num : num -> tm
-  | TM_ArrNew : typ -> tm -> tm
-  | TM_ArrIdx : tm -> tm -> tm
   | TM_Id : name -> tm
-  (* statements *)
+  | TM_Fun : name -> typ -> tm -> typ -> tm
+  | TM_New : tm -> tm
+  | TM_Loc : num -> tm
+  | TM_Load : tm -> tm
   | TM_Asg : tm -> tm -> tm
-  | TM_ArrAsg : tm -> tm -> tm -> tm
   | TM_Call : tm -> tm -> tm
   | TM_Seq : tm -> tm -> tm
-  (* concurrency statements *)
-  | TM_Spawn : tm -> tm
-  (* definitions *)
-  | TM_LetVal : name -> typ -> tm -> tm -> tm
-  | TM_LetVar : name -> typ -> tm -> tm -> tm
-  | TM_LetFun : name -> typ -> tm -> tm -> tm
-  (* internal terms *)
-  | TM_Loc : num -> tm
-  | TM_Arr : typ -> num -> tm (* TODO: ordering *)
-  | TM_Load : tm -> tm
-  | TM_Fun : name -> typ -> tm -> typ -> tm
+  | TM_Let : name -> typ -> tm -> tm -> tm
   .
 
 (* Values *)
 
 Inductive value : tm -> Prop :=
-  | V_Nil    : value TM_Nil
-  | V_Num    : forall n, value (TM_Num n)
-  | V_Arr    : forall T i, value (TM_Arr T i)
-  (* internal *)
+  | V_Nil : value TM_Nil
+  | V_Num : forall n, value (TM_Num n)
   | V_Loc : forall i, value (TM_Loc i)
   | V_Fun : forall p P block R, value (TM_Fun p P block R)
   .
@@ -105,10 +58,16 @@ Inductive value : tm -> Prop :=
 
 Inductive effect : Set :=
   | EF_None
-  | EF_Spawn (block : tm)
-  | EF_Alloc (addr : num) (t : tm)
-  | EF_Load  (addr : num) (t : tm)
-  | EF_Store (addr : num) (t : tm)
+  | EF_Alloc (ad : num) (t : tm)
+  | EF_Load  (ad : num) (t : tm)
+  | EF_Store (ad : num) (t : tm)
+  .
+
+Inductive ceffect : Set :=
+  | CEF_None  (tid : nat)
+  | CEF_Alloc (tid : nat) (ad : nat) (t : tm)
+  | CEF_Load  (tid : nat) (ad : nat) (t : tm)
+  | CEF_Store (tid : nat) (ad : nat) (t : tm)
   .
 
 (* Auxiliary Aliases *)
@@ -129,94 +88,57 @@ Fixpoint subst (id : name) (x t : tm) : tm :=
   match t with
   | TM_Nil => t
   | TM_Num _ => t
-  | TM_Arr _ _ => t
-  | TM_ArrNew T e =>
-      TM_ArrNew T ([id := x] e)
-  | TM_ArrIdx arr idx =>
-      TM_ArrIdx ([id := x] arr) ([id := x] idx)
   | TM_Id id' =>
       if id =? id' then x else t
+  | TM_Fun p P block R =>
+      if id =? p then t else TM_Fun p P ([id := x] block) R
+  | TM_New t' =>
+      [id := x] t'
+  | TM_Loc _ => t
+  | TM_Load t' =>
+      TM_Load ([id := x] t')
   | TM_Asg t' e =>
       TM_Asg ([id := x] t') ([id := x] e)
-  | TM_ArrAsg arr idx e =>
-      TM_ArrAsg ([id := x] arr) ([id := x] idx) ([id := x] e)
   | TM_Call f a =>
       TM_Call ([id := x] f) ([id := x] a)
   | TM_Seq t1 t2 =>
       TM_Seq ([id := x] t1) ([id := x] t2)
-  | TM_Spawn block =>
-      TM_Spawn ([id := x] block)
-  | TM_LetVal id' E e t' =>
-      TM_LetVal id' E ([id := x] e) (if id =? id' then t' else [id := x] t')
-  | TM_LetVar id' E e t' =>
-      TM_LetVar id' E ([id := x] e) (if id =? id' then t' else [id := x] t')
-  | TM_LetFun id' F f t' => 
-      TM_LetFun id' F ([id := x] f) (if id =? id' then t' else [id := x] t')
-  | TM_Loc _ => t
-  | TM_Load t' =>
-      TM_Load ([id := x] t')
-  | TM_Fun p P block R =>
-      if id =? p then t else TM_Fun p P ([id := x] block) R
+  | TM_Let id' E e t' =>
+      TM_Let id' E ([id := x] e) (if id =? id' then t' else [id := x] t')
   end
   where "'[' id ':=' x ']' t" := (subst id x t).
 
 Inductive step : tm -> effect -> tm -> Prop :=
   (* ArrNew *)
-  | ST_ArrNew1 : forall T e e' eff,
-    e --[eff]--> e' ->
-    TM_ArrNew T e --[eff]--> TM_ArrNew T e'
+  | ST_New1 : forall t t' eff,
+    t --[eff]--> t' ->
+    TM_New t --[eff]--> t'
 
-  | ST_ArrNew : forall T addr v,
-    value v ->
-    TM_ArrNew T v --[EF_Alloc addr v]--> TM_Arr T addr
+  | ST_ArrNew : forall t ad,
+    value t ->
+    TM_New t --[EF_Alloc ad t]--> TM_Loc ad
 
-  (* ArrIdx *)
-  | ST_ArrIdx1 : forall arr arr' idx eff,
-    arr --[eff]--> arr' ->
-    TM_ArrIdx arr idx --[eff]--> TM_ArrIdx arr' idx
+  (* Load *)
+  | ST_Load1 : forall t t' eff,
+    t --[eff]--> t' ->
+    TM_Load t --[eff]--> TM_Load t'
 
-  | ST_ArrIdx2 : forall arr idx idx' eff,
-    value arr ->
-    idx --[eff]--> idx' ->
-    TM_ArrIdx arr idx --[eff]--> TM_ArrIdx arr idx'
-
-  | ST_ArrIdx : forall T addr idx,
-    TM_ArrIdx (TM_Arr T addr) (TM_Num idx) --[EF_None]--> TM_Load (TM_Loc addr)
+  | ST_Load : forall ad t,
+    TM_Load (TM_Loc ad) --[EF_Load ad t]--> t
 
   (* Asg *)
-  | ST_Asg1 : forall t t' e eff,
-    t --[eff]--> t' ->
-    TM_Asg t e --[eff]--> TM_Asg t' e
+  | ST_Asg1 : forall l l' r eff,
+    l --[eff]--> l' ->
+    TM_Asg l r --[eff]--> TM_Asg l' r
 
-  | ST_Asg2 : forall t e e' eff,
+  | ST_Asg2 : forall l r r' eff,
+    value l ->
+    r --[eff]--> r' ->
+    TM_Asg l r --[eff]--> TM_Asg l r'
+
+  | ST_Asg : forall t ad,
     value t ->
-    e --[eff]--> e' ->
-    TM_Asg t e --[eff]--> TM_Asg t e'
-
-  | ST_Asg : forall addr v,
-    value v ->
-    TM_Asg (TM_Loc addr) v --[EF_Store addr v]--> TM_Nil
-
-  (* ArrAsg *)
-  | ST_ArrAsg1 : forall arr arr' idx e eff,
-    arr --[eff]--> arr' ->
-    TM_ArrAsg arr idx e --[eff]--> TM_ArrAsg arr' idx e
-
-  | ST_ArrAsg2 : forall arr idx idx' e eff,
-    value arr ->
-    idx --[eff]--> idx' ->
-    TM_ArrAsg arr idx e --[eff]--> TM_ArrAsg arr idx' e
-
-  | ST_ArrAsg3 : forall arr idx e e' eff,
-    value arr ->
-    value idx ->
-    e --[eff]--> e' ->
-    TM_ArrAsg arr idx e --[eff]--> TM_ArrAsg arr idx e'
-
-  | ST_ArrAsg : forall T addr idx v,
-    value v ->
-    value idx ->
-    TM_ArrAsg (TM_Arr T addr) idx v --[EF_None]--> TM_Asg (TM_Loc addr) v
+    TM_Asg (TM_Loc ad) t --[EF_Store ad t]--> TM_Nil
 
   (* Call *)
   | ST_Call1 : forall f f' a eff,
@@ -240,228 +162,90 @@ Inductive step : tm -> effect -> tm -> Prop :=
   | ST_Seq : forall t,
     TM_Seq TM_Nil t --[EF_None]--> t
 
-  (* Spawn *)
-  | ST_Spawn : forall block,
-    TM_Spawn block --[EF_Spawn block]--> TM_Nil
-
-  (* LetVal *)
-  | ST_LetVal1 : forall id E e e' t eff,
+  (* Let *)
+  | ST_Let1 : forall id E e e' t eff,
     e --[eff]--> e' ->
-    TM_LetVal id E e t --[eff]--> TM_LetVal id E e' t
+    TM_Let id E e t --[eff]--> TM_Let id E e' t
 
-  | ST_LetVal : forall id E e t,
+  | ST_Let : forall id E e t,
     value e ->
-    TM_LetVal id E e t --[EF_None]--> [id := e] t
-
-  (* LetVar *)
-  | ST_LetVar1 : forall id E e e' t eff,
-    e --[eff]--> e' ->
-    TM_LetVar id E e t --[eff]--> TM_LetVar id E e' t
-
-  | ST_LetVar : forall id E addr e t,
-    value e ->
-    TM_LetVar id E e t --[EF_Alloc addr e]--> [id := TM_Loc addr] t
-
-  (* LetFun *)
-  | ST_LetFun1 : forall id F f f' t eff,
-    f --[eff]--> f' ->
-    TM_LetFun id F f t --[eff]--> TM_LetFun id F f' t
-
-  | ST_LetFun : forall id F f t,
-    value f ->
-    TM_LetFun id F f t --[EF_None]--> [id := f] t
-
-  (* Load *)
-  | ST_Load1 : forall t t' eff,
-    t --[eff]--> t' ->
-    TM_Load t --[eff]--> TM_Load t'
-
-  | ST_Load : forall addr v,
-    TM_Load (TM_Loc addr) --[EF_Load addr v]--> v
+    TM_Let id E e t --[EF_None]--> [id := e] t
 
   where "t '--[' eff ']-->' t'" := (step t eff t').
 
 (* Concurrent Step *)
 
-Inductive ceffect : Set :=
-  | CEF_None  (i : num)
-  | CEF_Alloc (i : num) (addr : num)
-  | CEF_Load  (i : num) (addr : num)
-  | CEF_Store (i : num) (addr : num)
-  | CEF_Spawn (i : num)
-  .
-
 Inductive cstep : mem -> threads -> mem -> threads -> ceffect -> Prop :=
-  | CST_None : forall i m ths t,
-    i < length ths ->
-    (get_tm ths i) --[EF_None]--> t ->
-    m / ths ==> m / (set ths i t) # (CEF_None i)
+  | CST_None : forall tid m ths t,
+    tid < length ths ->
+    (get_tm ths tid) --[EF_None]--> t ->
+    m / ths ==[ CEF_None tid ]==> m / (set ths tid t)
 
-  | CST_Alloc : forall i m ths v t,
-    i < length ths ->
-    (get_tm ths i) --[EF_Alloc (length m) v]--> t ->
-    m / ths ==> (add m v) / (set ths i t) # (CEF_Alloc i (length m))
+  | CST_Alloc : forall tid m ths v t,
+    tid < length ths ->
+    (get_tm ths tid) --[EF_Alloc (length m) v]--> t ->
+    m / ths ==[ CEF_Alloc tid (length m) v ]==> (add m v) / (set ths tid t)
 
-  | CST_Load : forall i m ths addr t,
-    i < length ths ->
-    (get_tm ths i) --[EF_Load addr (get_tm m addr)]--> t ->
-    m / ths ==> m / (set ths i t) # (CEF_Load i addr)
+  | CST_Load : forall tid m ths ad t,
+    tid < length ths ->
+    (get_tm ths tid) --[EF_Load ad (get_tm m ad)]--> t ->
+    m / ths ==[ CEF_Load tid ad (get_tm m ad) ]==> m / (set ths tid t)
 
-  | CST_Store : forall i m ths addr v t,
-    i < length ths ->
-    addr < length m ->
-    (get_tm ths i) --[EF_Store addr v]--> t ->
-    m / ths ==> (set m addr v) / (set ths i t) # (CEF_Store i addr)
+  | CST_Store : forall tid m ths ad v t,
+    tid < length ths ->
+    ad < length m ->
+    (get_tm ths tid) --[EF_Store ad v]--> t ->
+    m / ths ==[ CEF_Store tid ad v ]==> (set m ad v) / (set ths tid t)
 
-  | CST_Spawn : forall i m ths block t,
-    i < length ths ->
-    (get_tm ths i) --[EF_Spawn block]--> t ->
-    m / ths ==> m / (add (set ths i t) block) # (CEF_Spawn i)
-
-  where "m / ths '==>' m' / ths' # ceff" := (cstep m ths m' ths' ceff).
-
-function f (m th addr) =>
-  se a thread tem TM_Loc addr
-    ret true
-  senão
-    ret false
-
-Loc 2 = x
-
-(Loc 2)[2] = x
-
-
-Inductive rel : m -> tm -> nat : Prop :=
-  | TM_ArrNew : typ -> tm -> tm
-  | TM_ArrIdx : tm -> tm -> tm
-  (* statements *)
-  | TM_Asg : tm -> tm -> tm
-  | TM_ArrAsg : tm -> tm -> tm -> tm
-  | TM_Call : tm -> tm -> tm
-  | TM_Seq : tm -> tm -> tm
-  (* concurrency statements *)
-  | TM_Spawn : tm -> tm
-  (* definitions *)
-  | TM_LetVal : name -> typ -> tm -> tm -> tm
-  | TM_LetVar : name -> typ -> tm -> tm -> tm
-  | TM_LetFun : name -> typ -> tm -> tm -> tm
-  (* internal terms *)
-  | R_Loc : forall m loc,
-      rel m (TM_Loc loc) loc
-  | TM_Load : tm -> tm
-  | TM_Fun : name -> typ -> tm -> typ -> tm
-
-
-  | R_Trans : forall m t loc loc',
-    rel m t loc ->
-    m[loc] = TM_Loc loc' ->
-    rel m t loc'
-
-  .
-
-
-
-
-Inductive something : 
-  | Something_Load
-    tid = alguma thread
-    m / ths ==> m' / ths' # Load tid 23
-    em todas as outras threads que não são tid,
-    não pode ter Loc 23
-    
-
-
+  where "m / ths '==[' ceff ']==>' m' / ths'" := (cstep m ths m' ths' ceff).
 
 (* Typing *)
 
-Inductive well_typed_term {mt : memtyp} : ctx -> tm -> typ -> Prop :=
+Inductive well_typed_term (mt : memtyp) : ctx -> tm -> typ -> Prop :=
   | T_Nil : forall Gamma,
-    Gamma |-- TM_Nil is TY_Void
+    mt / Gamma |-- TM_Nil is TY_Void
 
   | T_Num : forall Gamma n,
-    Gamma |-- (TM_Num n) is TY_Num
+    mt / Gamma |-- (TM_Num n) is TY_Num
 
-  | T_Arr : forall Gamma T i,
-    T = get_typ mt i ->
-    i < length mt ->
-    Gamma |-- (TM_Arr (TY_Arr T) i) is (TY_Arr T)
-
-  | T_IArr : forall Gamma T i,
-    T = get_typ mt i ->
-    i < length mt ->
-    Gamma |-- (TM_Arr (TY_IArr T) i) is (TY_IArr T)
-
-  | T_ArrNew : forall Gamma e E,
-    Gamma |-- e is E ->
-    Gamma |-- (TM_ArrNew (TY_Arr E) e) is (TY_Arr E)
-
-  | T_ArrIdx : forall Gamma arr idx T,
-    Gamma |-- arr is (TY_Arr T) ->
-    Gamma |-- idx is TY_Num ->
-    Gamma |-- (TM_ArrIdx arr idx) is T
-
-  | T_Id_Val : forall Gamma id T,
-    lookup Gamma id = Some (TY_IRef T) ->
-    Gamma |-- (TM_Id id) is (TY_IRef T)
-
-  | T_Id_Var : forall Gamma id T,
-    lookup Gamma id = Some (TY_Ref T) ->
-    Gamma |-- (TM_Id id) is (TY_Ref T)
-
-  | T_Asg : forall Gamma t e T,
-    Gamma |-- t is (TY_Ref T) ->
-    Gamma |-- e is T ->
-    Gamma |-- (TM_Asg t e) is TY_Void
-
-  | T_ArrAsg : forall Gamma arr idx e E,
-    Gamma |-- arr is (TY_Arr E) ->
-    Gamma |-- idx is TY_Num ->
-    Gamma |-- e is E ->
-    Gamma |-- (TM_ArrAsg arr idx e) is TY_Void
-
-  | T_Call : forall Gamma f a P R,
-    Gamma |-- f is (TY_Fun P R) ->
-    Gamma |-- a is P ->
-    Gamma |-- (TM_Call f a) is R
-
-  | T_Seq : forall Gamma t t' T,
-    Gamma |-- t  is TY_Void ->
-    Gamma |-- t' is T ->
-    Gamma |-- (TM_Seq t t') is T
-
-  | T_Spawn : forall Gamma block T,
-    safe Gamma |-- block is T ->
-    Gamma |-- (TM_Spawn block) is TY_Void
-
-  | T_LetVal : forall Gamma id e t E T,
-    Gamma |-- e is E ->
-    (update Gamma id E) |-- t is T ->
-    Gamma |-- (TM_LetVal id E e t) is T
-
-  | T_LetVar : forall Gamma id e t E T,
-    Gamma  |-- e is E ->
-    (update Gamma id (TY_Ref E)) |-- t is T ->
-    Gamma  |-- (TM_LetVar id E e t) is T
-
-  | T_LetFun : forall Gamma id F P R f t T,
-    F = TY_Fun P R ->
-    safe Gamma |-- f is F ->
-    (update Gamma id F) |-- t is T ->
-    Gamma |-- (TM_LetFun id F f t) is T
-
-  | T_Loc : forall Gamma i,
-    i < length mt ->  
-    mt / Gamma |-- (TM_Loc i) is TY_Ref (get_typ mt i)
-
-  | T_Load : forall Gamma t T,
-    Gamma |-- t is TY_Ref T ->
-    Gamma |-- (TM_Load t) is T
-
-Load (Loc 123)
+  | T_Id : forall Gamma id T,
+    lookup Gamma id = Some T ->
+    mt / Gamma |-- (TM_Id id) is T
 
   | T_Fun : forall Gamma p P block R,
-    (update Gamma p P) |-- block is R ->
-    Gamma |-- (TM_Fun p P block R) is (TY_Fun P R)
+    mt / (update Gamma p P) |-- block is R ->
+    mt / Gamma |-- (TM_Fun p P block R) is (TY_Fun P R)
 
-  where "Gamma '|--' t 'is' T" := (well_typed_term Gamma t T)
-  and "mt / Gamma '|--' t 'is' T" := (@well_typed_term mt Gamma t T).
+  | T_New : forall Gamma t T,
+    mt / Gamma |-- t is T ->
+    mt / Gamma |-- (TM_New t) is (TY_Ref T)
+
+  | T_Loc : forall Gamma ad,
+    ad < length mt ->
+    mt / Gamma |-- (TM_Loc ad) is TY_Ref (get_typ mt ad)
+
+  | T_Load : forall Gamma t T,
+    mt / Gamma |-- t is TY_Ref T ->
+    mt / Gamma |-- (TM_Load t) is T
+
+  | T_Asg : forall Gamma t e T,
+    mt / Gamma |-- t is (TY_Ref T) ->
+    mt / Gamma |-- e is T ->
+    mt / Gamma |-- (TM_Asg t e) is TY_Void
+
+  | T_Call : forall Gamma f a P R,
+    mt / Gamma |-- f is (TY_Fun P R) ->
+    mt / Gamma |-- a is P ->
+    mt / Gamma |-- (TM_Call f a) is R
+
+  | T_Seq : forall Gamma t t' T,
+    mt / Gamma |-- t  is TY_Void ->
+    mt / Gamma |-- t' is T ->
+    mt / Gamma |-- (TM_Seq t t') is T
+
+  | T_Let : forall Gamma id e t E T,
+    mt / Gamma |-- e is E ->
+    mt / (update Gamma id E) |-- t is T ->
+    mt / Gamma |-- (TM_Let id E e t) is T
+
+  where "mt / Gamma '|--' t 'is' T" := (well_typed_term mt Gamma t T).
--- Safety.v
+++ Safety.v
@@ -1,310 +1,68 @@
-From Coq Require Import Init.Nat.
-From Coq Require Import Arith.Arith.
-From Coq Require Import Lists.List.
 
-From Elo Require Export Array.
-From Elo Require Export Multimap.
+
 From Elo Require Export Core.
-From Elo Require Export Properties.
 
-Reserved Notation "m / ths / mvis / trc '~~>' m' / ths' / mvis' / trc'"
-  (at level 40, ths at next level, mvis at next level, trc at next level,
-                m' at next level, ths' at next level, mvis' at next level).
+Inductive has_access : tm -> nat -> Prop :=
+  | has_access_trans : forall ad ad' m t,
+    get_tm m ad = TM_Loc ad' ->
+    has_access t ad ->
+    has_access t ad'
 
-(* memory visibility *)
-(* maps a memory address to the threads that can access it *)
-Definition memvis := multimap nat nat.
-Definition trace := list ceffect.
+  | has_access_fun : forall ad p P block R,
+    has_access block ad ->
+    has_access (TM_Fun p P block R) ad
 
-Definition memvis_empty : memvis := multimap_empty eqb.
+  | has_access_new : forall ad t,
+    has_access t ad ->
+    has_access (TM_New t) ad
 
-Definition memvis_update mvis ceff :=
-  match ceff with
-  | CEF_None  tid => mvis
-  | CEF_Alloc tid addr => multimap_update mvis addr tid
-  | CEF_Load  tid addr => mvis
-  | CEF_Store tid addr => mvis
-  | CEF_Spawn tid => mvis (* TODO *)
-  end.
+  | has_access_loc : forall ad,
+    has_access (TM_Loc ad) ad
 
-(* ------------------------------------------------------------------------- *)
+  | has_access_load : forall ad t,
+    has_access t ad ->
+    has_access (TM_Load t) ad
 
-(*
-Reserved Notation "mt / Gamma '|==' t 'is' mvis"
-  (at level 40, Gamma at next level).
+  | has_access_asg1 : forall ad l r,
+    has_access l ad ->
+    has_access (TM_Asg l r) ad
 
-Inductive well_typed_term' (mt : memtyp) : ctx -> tm -> memvis -> Prop :=
-  
-(*
-  | T_Arr : forall Gamma T i,
-    T = get_typ mt i ->
-    i < length mt ->
-    Gamma |-- (TM_Arr (TY_Arr T) i) is (TY_Arr T)
+  | has_access_asg2 : forall ad l r,
+    has_access r ad ->
+    has_access (TM_Asg l r) ad
 
-  | T_IArr : forall Gamma T i,
-    T = get_typ mt i ->
-    i < length mt ->
-    Gamma |-- (TM_Arr (TY_IArr T) i) is (TY_IArr T)
+  | has_access_call1 : forall ad f a,
+    has_access f ad ->
+    has_access (TM_Call f a) ad
 
-  | T_ArrNew : forall Gamma e E,
-    Gamma |-- e is E ->
-    Gamma |-- (TM_ArrNew (TY_Arr E) e) is (TY_Arr E)
+  | has_access_call2 : forall ad f a,
+    has_access a ad ->
+    has_access (TM_Call f a) ad
 
-  | T_IArrNew : forall Gamma e E,
-    Gamma |-- e is E -> (* immutable *)
-    Gamma |-- (TM_ArrNew (TY_IArr E) e) is (TY_IArr E)
+  | has_access_seq1 : forall ad t1 t2,
+    has_access t1 ad ->
+    has_access (TM_Seq t1 t2) ad
 
-  | T_ArrIdx : forall Gamma arr idx T,
-    Gamma |-- arr is (TY_Arr T) ->
-    Gamma |-- idx is TY_Num ->
-    Gamma |-- (TM_ArrIdx arr idx) is T
+  | has_access_seq2 : forall ad t1 t2,
+    has_access t2 ad ->
+    has_access (TM_Seq t1 t2) ad
 
-  | T_IArrIdx : forall Gamma arr idx T,
-    Gamma |-- arr is (TY_IArr T) ->
-    Gamma |-- idx is TY_Num ->
-    Gamma |-- (TM_ArrIdx arr idx) is T
+  | has_access_let1 : forall ad id E e t,
+    has_access e ad ->
+    has_access (TM_Let id E e t) ad
 
-  | T_Id_Val : forall Gamma id T,
-    lookup Gamma id = Some (TY_IRef T) ->
-    Gamma |-- (TM_Id id) is (TY_IRef T)
-
-  | T_Id_Var : forall Gamma id T,
-    lookup Gamma id = Some (TY_Ref T) ->
-    Gamma |-- (TM_Id id) is (TY_Ref T)
-
-  | T_Asg : forall Gamma t e T,
-    Gamma |-- t is (TY_Ref T) ->
-    Gamma |-- e is T ->
-    Gamma |-- (TM_Asg t e) is TY_Void
-
-  | T_ArrAsg : forall Gamma arr idx e E,
-    Gamma |-- arr is (TY_Arr E) ->
-    Gamma |-- idx is TY_Num ->
-    Gamma |-- e is E ->
-    Gamma |-- (TM_ArrAsg arr idx e) is TY_Void
-
-  | T_Call : forall Gamma f a P R,
-    Gamma |-- f is (TY_Fun P R) ->
-    Gamma |-- a is P ->
-    Gamma |-- (TM_Call f a) is R
-
-  | T_Seq : forall Gamma t t' T,
-    Gamma |-- t  is TY_Void ->
-    Gamma |-- t' is T ->
-    Gamma |-- (TM_Seq t t') is T
-
-  | T_Spawn : forall Gamma block T,
-    safe Gamma |-- block is T ->
-    Gamma |-- (TM_Spawn block) is TY_Void
-
-  | T_LetVal : forall Gamma id e t E T,
-    Gamma |-- e is E ->
-    (update Gamma id E) |-- t is T ->
-    Gamma |-- (TM_LetVal id E e t) is T
-
-  | T_LetVar : forall Gamma id e t E T,
-    Gamma  |-- e is E ->
-    (update Gamma id (TY_Ref E)) |-- t is T ->
-    Gamma  |-- (TM_LetVar id E e t) is T
-
-  | T_LetFun : forall Gamma id F P R f t T,
-    F = TY_Fun P R ->
-    safe Gamma |-- f is F ->
-    (update Gamma id F) |-- t is T ->
-    Gamma |-- (TM_LetFun id F f t) is T
-
-  | T_Loc : forall Gamma i,
-    i < length mt ->
-    Gamma |-- (TM_Loc i) is TY_Ref (get_typ mt i)
-*)
-  | T_Load_Ref : forall Gamma t T,
-    mt / Gamma |-- t is TY_Ref T ->
-    mt / Gamma |== (TM_Load t) is T
-
-  | T_Load_IRef : forall Gamma t T,
-    Gamma |-- t is TY_IRef T ->
-    Gamma |-- (TM_Load t) is T
-*)
-
-Fixpoint f t : list nat :=
-  match t with
-  | TM_Nil => nil
-  | TM_Num _ => nil
-  | TM_Load t' => f t'
-  | TM_Fun p P block R => f block
-  | _ => nil
-  end.
-
-
-(* ------------------------------------------------------------------------- *)
-
-==>
-
-(* trace step (from one concurrent state to another) *)
-Inductive tstep : mem -> threads -> memvis -> trace ->
-                  mem -> threads -> memvis -> trace -> Prop :=
-  | trace_refl : forall m ths mvis trc,
-    m / ths / mvis / trc ~~> m / ths / mvis / trc
-
-  | trace_step : forall m0 m m' ths0 ths ths' mvis0 mvis trc0 trc ceff,
-    m0 / ths0 / mvis0 / trc0 ~~> m / ths / mvis / trc ->
-    m / ths ==> m' / ths' # ceff ->
-    m / ths / mvis / trc ~~> m' / ths' / (memvis_update mvis ceff) / (ceff :: trc)
-
-  where "m / ths / mvis / trc '~~>' m' / ths' / mvis' / trc'" :=
-    (tstep m ths mvis trc m' ths' mvis' trc').
-
-Definition safe_memory_address mt addr :=
-  safe_type (get_typ mt addr).
-
-Definition safe_load mt (mvis : memvis) tid addr :=
-  safe_memory_address mt addr \/
-  multimap_lookup mvis addr = (tid :: nil).
-
-Definition safe_store (mvis : memvis) tid addr :=
-  multimap_lookup mvis addr = (tid :: nil).
-
-Inductive well_formed_trace : memtyp -> memvis -> trace -> Prop :=
-  | well_formed_trace_nil : forall mt, (* TODO: is this mt correct? *)
-    well_formed_trace mt memvis_empty nil
-
-  | well_formed_trace_none : forall mt mvis trc tid,
-    well_formed_trace mt mvis trc ->
-    well_formed_trace mt mvis (CEF_None tid :: trc)
-
-  | well_formed_trace_alloc : forall mt mt' T mvis mvis' trc tid addr,
-    mt' = add mt T ->
-    mvis' = multimap_update mvis addr tid ->
-    well_formed_trace mt mvis trc ->
-    well_formed_trace mt' mvis' (CEF_Alloc tid addr :: trc)
-
-  | well_formed_trace_load : forall mt mvis trc tid addr,
-    safe_load mt mvis tid addr ->
-    well_formed_trace mt mvis trc ->
-    well_formed_trace mt mvis (CEF_Load tid addr :: trc)
-
-  | well_formed_trace_store : forall mt mvis trc tid addr,
-    safe_store mvis tid addr ->
-    well_formed_trace mt mvis trc ->
-    well_formed_trace mt mvis (CEF_Store tid addr :: trc)
-
-  | well_formed_trace_spawn : forall mt mvis trc tid,
-    well_formed_trace mt mvis trc ->
-    well_formed_trace mt mvis (CEF_Spawn tid :: trc) (* TODO: different mvis *)
+  | has_access_let2 : forall ad id E e t,
+    has_access t ad ->
+    has_access (TM_Let id E e t) ad
   .
 
-(* TODO: move *)
-Theorem cstep_none_preservation : forall mt m ths ths' tid,
-  well_typed_program mt m ths ->
-  m / ths ==> m / ths' # (CEF_None tid) ->
-  well_typed_program mt m ths'.
-Proof.
-  intros * [Hmem Hths] Hcstep.
-  inversion Hcstep; subst. split; trivial.
-  intros tid'. destruct (tid =? tid') eqn:E.
-  + apply Nat.eqb_eq in E. subst.
-    rewrite (get_i_set_i TM_Nil); trivial.
-    specialize (Hths tid') as [? ?]. eexists.
-    eauto using limited_preservation.
-  + apply Nat.eqb_neq in E. apply not_eq_sym in E.
-    rewrite (get_i_set_j TM_Nil); trivial.
-Qed.
-
-Theorem safety_preservation : forall mt m m' ths ths' mvis mvis' trc trc',
-  well_typed_program mt m ths ->
-  well_formed_trace mt mvis trc ->
-  m / ths / mvis / trc ~~> m' / ths' / mvis' / trc' ->
-  exists mt' mvis',
-    mt' extends mt /\
-    well_formed_trace mt' mvis' trc'.
-Proof.
-  intros * Hwt Htrace Htstep. inversion Htstep; subst.
-  - eexists. eexists. split; eauto using extends_refl.
-  - unfold memvis_update in *. destruct ceff.
-    + inversion H0; subst. eauto.
-      assert (well_typed_program mt m' (set ths i t)) as [? Hwt'].
-      eauto using cstep_none_preservation.
-      eexists. eexists. split; eauto using extends_refl, well_formed_trace.
-    + eexists. eexists. split; eauto using extends_add.
-      eapply well_formed_trace_alloc; eauto.
-    + eexists. eexists. split; eauto using extends_refl.
-      eapply well_formed_trace_load; eauto.
-      unfold safe_load.
-      specialize Hwt as [Hmem Hths].
-Admitted.
-
-
 (*
-Lemma well_typed_cmultistep : forall m m' ths ths' mvis trc,
-  m / ths ==>* m' / ths' # mvis # trc ->
-  exists mt, well_typed_program mt m' ths'.
-Proof.
-  intros * Hcmultistep. induction Hcmultistep;
-  try solve [
-    match goal with
-    | IH : exists mt, _ |- _ =>
-      specialize IH as [mt ?]
-    end;
-    assert (exists mt',
-      mt' extends mt /\
-      well_typed_program mt' m' ths') as [? [_ ?]];
-    eauto using preservation
-  ].
-  exists nil. split.
-  - split; trivial. destruct i; eauto using @typeof.
-  - intros i. unfold get_tm. simpl.
-    repeat (destruct i; eauto using @typeof).
-Qed.
-*)
 
-(*
-Lemma aux : forall mt m0 m m' ths0 ths ths' mvis trc tid addr,
-  m0 / ths0 ==>* m / ths # mvis # trc ->
-  well_typed_memory mt m ->
-  mt |== trc ->
-  m / ths ==> m' / ths' # CEF_Load tid addr ->
-  exists mt',
-    well_typed_memory mt' m' /\
-    mt' |== (CEF_Load tid addr :: trc).
-Proof.
-  intros * Hcmultistep Hmem Htrace Hcstep.
-  eexists mt. split.
-  - inversion Hcstep. subst. assumption.
-  - eapply well_formed_trace_load; eauto.
-    unfold safe_load.
-Admitted.
-
-Theorem traces_are_well_formed : forall m ths mvis trace,
-  m / ths ==>* mvis / trace ->
-  |== trace.
-Proof.
-  intros * Hcsteps. induction Hcsteps; eauto using well_formed_trace.
-  - eapply well_formed_trace_load; eauto.
-  - eapply well_formed_trace_store; eauto.
-Admitted.
-*)
-
-(* FOR LATER *)
-
-(*
-Lemma well_typed_cmultistep : forall m m' ths ths' mvis trc,
-  m / ths ==>* m' / ths' # mvis # trc ->
-  exists mt, well_typed_program mt m' ths'.
-Proof.
-  intros * Hcmultistep. induction Hcmultistep;
-  try solve [
-    match goal with
-    | IH : exists mt, _ |- _ =>
-      specialize IH as [mt ?]
-    end;
-    assert (exists mt',
-      mt' extends mt /\
-      well_typed_program mt' m' ths') as [? [_ ?]];
-    eauto using preservation
-  ].
-  exists nil. split.
-  - split; trivial. destruct i; eauto using @typeof.
-  - intros i. unfold get_tm. simpl.
-    repeat (destruct i; eauto using @typeof).
-Qed.
+Inductive something : 
+  | Something_Load
+    tid = alguma thread
+    m / ths ==> m' / ths' # Load tid 23
+    em todas as outras threads que não são tid,
+    não pode ter Loc 23
+    
 *)
