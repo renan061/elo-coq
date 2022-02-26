3b59c7d8e3be2d5673b4064af61a226f32bc9a5e
Renan Santos <renan.061@gmail.com>
2022-02-26 00:18:00 -0300

    Simplifying the language (a lot)

---
+++ Core0.v
@@ -0,0 +1,151 @@
+From Coq Require Import Init.Nat.
+From Coq Require Import List.
+
+From Elo Require Export Array.
+From Elo Require Export Map.
+
+Import ListNotations.
+
+Definition name := String.string.
+Definition num := nat.
+
+(* Notations *)
+
+Reserved Notation "t '--[' eff ']-->' t'"
+  (at level 40).
+Reserved Notation "m / ths '==[' ceff ']==>' m' / ths'"
+  (at level 40, ths at next level, ceff at next level, m' at next level).
+Reserved Notation "mt / Gamma '|--' t 'is' T"
+  (at level 40, Gamma at next level).
+
+(* Types *)
+
+Inductive typ : Set :=
+  | TY_Void
+  | TY_Num
+  | TY_Ref : typ -> typ
+  .
+
+(* Terms *)
+
+Inductive tm : Set :=
+  | TM_Nil
+  | TM_Num  : num -> tm
+  | TM_Loc  : num -> tm
+  | TM_New  : tm -> tm
+  | TM_Load : tm -> tm
+  | TM_Asg  : tm -> tm -> tm
+  .
+
+(* Values *)
+
+Inductive value : tm -> Prop :=
+  | V_Nil : value TM_Nil
+  | V_Num : forall n, value (TM_Num n)
+  | V_Loc : forall i, value (TM_Loc i)
+  .
+
+(* Effects *)
+
+Definition addr := nat.
+
+Inductive effect : Set :=
+  | EF_None
+  | EF_Alloc (ad : addr) (t : tm)
+  | EF_Load  (ad : addr) (t : tm)
+  | EF_Store (ad : addr) (t : tm)
+  .
+
+(* Auxiliary Aliases *)
+
+Definition ctx := map typ.
+Definition mem := list tm.
+(* Definition threads := list tm. *)
+Definition memtyp := list typ.
+Definition get_typ := get TY_Void.
+Definition get_tm  := get TM_Nil.
+Definition get_ctx := get (@empty typ).
+
+(* Operational Semantics *)
+
+Local Infix "=?" := String.eqb (at level 70, no associativity).
+
+Inductive step : tm -> effect -> tm -> Prop :=
+  (* New *)
+  | ST_New1 : forall t t' eff,
+    t --[eff]--> t' ->
+    TM_New t --[eff]--> t'
+
+  | ST_New : forall t ad,
+    value t ->
+    TM_New t --[EF_Alloc ad t]--> TM_Loc ad
+
+  (* Load *)
+  | ST_Load1 : forall t t' eff,
+    t --[eff]--> t' ->
+    TM_Load t --[eff]--> TM_Load t'
+
+  | ST_Load : forall ad t,
+    TM_Load (TM_Loc ad) --[EF_Load ad t]--> t
+
+  (* Asg *)
+  | ST_Asg1 : forall l l' r eff,
+    l --[eff]--> l' ->
+    TM_Asg l r --[eff]--> TM_Asg l' r
+
+  | ST_Asg2 : forall l r r' eff,
+    value l ->
+    r --[eff]--> r' ->
+    TM_Asg l r --[eff]--> TM_Asg l r'
+
+  | ST_Asg : forall t ad,
+    value t ->
+    TM_Asg (TM_Loc ad) t --[EF_Store ad t]--> TM_Nil
+
+  where "t '--[' eff ']-->' t'" := (step t eff t').
+
+(* Memory Step *)
+
+Inductive mstep : mem -> tm -> effect -> mem -> tm -> Prop :=
+  | MST_Alloc : forall m t t' v,
+    t --[EF_Alloc (length m) v]--> t' ->
+    m / t ==[ EF_Alloc (length m) v ]==> (add m v) / t'
+
+  | MST_Load : forall m t t' ad,
+    t --[EF_Load ad (get_tm m ad)]--> t' ->
+    m / t ==[ EF_Load ad (get_tm m ad) ]==> m / t'
+
+  | MST_Store : forall m t t' ad v,
+    ad < length m ->
+    t --[EF_Store ad v]--> t' ->
+    m / t ==[ EF_Store ad v ]==> (set m ad v) / t'
+
+  where "m / t '==[' eff ']==>' m' / t'" := (mstep m t eff m' t').
+
+(* Typing *)
+
+Inductive well_typed_term (mt : memtyp) : ctx -> tm -> typ -> Prop :=
+  | T_Nil : forall Gamma,
+    mt / Gamma |-- TM_Nil is TY_Void
+
+  | T_Num : forall Gamma n,
+    mt / Gamma |-- (TM_Num n) is TY_Num
+
+  | T_Loc : forall Gamma ad,
+    ad < length mt ->
+    mt / Gamma |-- (TM_Loc ad) is TY_Ref (get_typ mt ad)
+
+  | T_New : forall Gamma t T,
+    mt / Gamma |-- t is T ->
+    mt / Gamma |-- (TM_New t) is (TY_Ref T)
+
+  | T_Load : forall Gamma t T,
+    mt / Gamma |-- t is TY_Ref T ->
+    mt / Gamma |-- (TM_Load t) is T
+
+  | T_Asg : forall Gamma l r T,
+    mt / Gamma |-- l is (TY_Ref T) ->
+    mt / Gamma |-- r is T ->
+    mt / Gamma |-- (TM_Asg l r) is TY_Void
+
+  where "mt / Gamma '|--' t 'is' T" := (well_typed_term mt Gamma t T).
--- Safety.v
+++ Safety.v
@@ -1,60 +1,106 @@
+From Coq Require Import Arith.Arith.
+From Coq Require Import Lists.List.
+From Coq Require Import Logic.ClassicalFacts.
 
+From Elo Require Export Core0.
 
-From Elo Require Export Core.
+Reserved Notation "m / t / tc '==>*' m' / t' / tc'"
+  (at level 40, t at next level, tc at next level,
+                m' at next level, t' at next level).
 
-Inductive has_access : tm -> nat -> Prop :=
-  | has_access_trans : forall ad ad' m t,
+Definition trace := list effect.
+
+Inductive has_access (m : mem) : tm -> addr -> Prop :=
+  | has_access_trans : forall t ad ad',
     get_tm m ad = TM_Loc ad' ->
-    has_access t ad ->
-    has_access t ad'
-
-  | has_access_fun : forall ad p P block R,
-    has_access block ad ->
-    has_access (TM_Fun p P block R) ad
-
-  | has_access_new : forall ad t,
-    has_access t ad ->
-    has_access (TM_New t) ad
+    has_access m t ad ->
+    has_access m t ad'
 
   | has_access_loc : forall ad,
-    has_access (TM_Loc ad) ad
+    has_access m (TM_Loc ad) ad
 
-  | has_access_load : forall ad t,
-    has_access t ad ->
-    has_access (TM_Load t) ad
+  | has_access_new : forall t ad,
+    has_access m t ad ->
+    has_access m (TM_New t) ad
 
-  | has_access_asg1 : forall ad l r,
-    has_access l ad ->
-    has_access (TM_Asg l r) ad
+  | has_access_load : forall t ad,
+    has_access m t ad ->
+    has_access m (TM_Load t) ad
 
-  | has_access_asg2 : forall ad l r,
-    has_access r ad ->
-    has_access (TM_Asg l r) ad
+  | has_access_asg1 : forall l r ad,
+    has_access m l ad ->
+    has_access m (TM_Asg l r) ad
 
-  | has_access_call1 : forall ad f a,
-    has_access f ad ->
-    has_access (TM_Call f a) ad
-
-  | has_access_call2 : forall ad f a,
-    has_access a ad ->
-    has_access (TM_Call f a) ad
-
-  | has_access_seq1 : forall ad t1 t2,
-    has_access t1 ad ->
-    has_access (TM_Seq t1 t2) ad
-
-  | has_access_seq2 : forall ad t1 t2,
-    has_access t2 ad ->
-    has_access (TM_Seq t1 t2) ad
-
-  | has_access_let1 : forall ad id E e t,
-    has_access e ad ->
-    has_access (TM_Let id E e t) ad
-
-  | has_access_let2 : forall ad id E e t,
-    has_access t ad ->
-    has_access (TM_Let id E e t) ad
+  | has_access_asg2 : forall l r ad,
+    has_access m r ad ->
+    has_access m (TM_Asg l r) ad
   .
+
+Theorem aux1 : forall m t t' ad v,
+  t' --[EF_Alloc ad v]--> t ->
+  has_access m t ad.
+Proof.
+  intros ? ? ?. generalize dependent t.
+  induction t'; intros * Hstep;
+  inversion Hstep; eauto using has_access.
+Qed.
+
+Inductive multistep : mem -> tm -> trace -> mem -> tm -> trace -> Prop :=
+  | multistep_first : forall main ad,
+    ~ (has_access nil main ad) ->
+    nil / TM_Nil / nil ==>* nil / main / (EF_None :: nil)
+
+  | multistep_step : forall m m' m'' t t' t'' tc tc' ceff,
+    m  / t / tc ==>* m' / t' / tc' ->
+    m' / t' ==[ceff]==> m'' / t'' ->
+    m  / t / tc ==>* m'' / t'' / (ceff :: tc')
+
+  where "m / t / tc '==>*' m' / t' / tc'" := (multistep m t tc m' t' tc').
+
+
+
+
+
+
+
+
+
+
+Lemma ceff_eq_dec : forall (x y : ceffect),
+  x = y \/ x <> y.
+Admitted.
+
+Theorem todo : forall m_ m ths_ ths tc_ tc tid ad v,
+  m_ / ths_ / tc_ ==>* m / ths / (CEF_Load tid ad v :: tc) ->
+  In (CEF_Alloc tid ad v) tc.
+Proof.
+  intros * Hmstep.
+  inversion Hmstep. subst.
+
+
+
+
+induction tc as [| ceff tc' IH].
+  - inversion Hmstep; subst. inversion H6.
+  - destruct (ceff_eq_dec ceff (CEF_Alloc tid ad v)); subst.
+    + eapply in_eq.
+    + eapply in_cons. clear H.
+      inversion Hmstep. subst.
+      inversion H6; subst.
+      * shelve.
+      *
+
+
+      inversion Hmstep. subst. clear Hmstep.
+      induction H7.
+      * inversion H8. subst.
+      eapply IH. clear IH.
+
+Qed.
+
+
+
+
 
 (*
 
